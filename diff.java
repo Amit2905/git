21403edb7594df2e45d0aac7d48b53ac2039dac9
Amit Surkar <amitsurkar1232@gmail.com>
2022-03-31 11:18:51 +0530

    diff modified file

--- diff.java
+++ diff.java
@@ -1,5 +1,9 @@
+import javax.swing.plaf.synth.SynthStyle;
+
 public class diff {
     public static void main(String[] args) {
-        
+      String str = "Amit Surkar";   
+
+      System.out.println(str);
     }
 }
